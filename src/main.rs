111dbfb53caee58d507f26c1cadb641a23cb25d6
Mario Zupan <mario@zupzup.org>
2024-04-16 23:16:31 +0200

    ui basics

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,34 @@ fn main() -> Result<(), eframe::Error> {
     // Our application state:
     let mut name = "Arthur".to_owned();
     let mut age = 42;
+    let wlan_interfaces = ["wlan0", "wlan1", "wlan2"];
+    let mut selected_wlan_interface = wlan_interfaces[0];
 
-    eframe::run_simple_native("My egui App", options, move |ctx, _frame| {
+    eframe::run_simple_native("Swelfie", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("My egui dd");
+            ui.heading("Swelfie");
+            ui.horizontal(|ui| {
+                ui.add(egui::Label::new("Select WLAN Interface"));
+                egui::ComboBox::from_label("")
+                    .selected_text(format!("{:?}", selected_wlan_interface))
+                    .show_ui(ui, |ui| {
+                        ui.selectable_value(
+                            &mut selected_wlan_interface,
+                            wlan_interfaces[0],
+                            wlan_interfaces[0],
+                        );
+                        ui.selectable_value(
+                            &mut selected_wlan_interface,
+                            wlan_interfaces[1],
+                            wlan_interfaces[1],
+                        );
+                        ui.selectable_value(
+                            &mut selected_wlan_interface,
+                            wlan_interfaces[2],
+                            wlan_interfaces[2],
+                        );
+                    });
+            });
             ui.horizontal(|ui| {
                 let name_label = ui.label("Your name: ");
                 ui.text_edit_singleline(&mut name)
@@ -29,6 +53,23 @@ fn main() -> Result<(), eframe::Error> {
                 age += 1;
             }
             ui.label(format!("Hello '{name}', age {age}"));
+            ui.vertical(|ui| {
+                ui.selectable_value(
+                    &mut selected_wlan_interface,
+                    wlan_interfaces[0],
+                    wlan_interfaces[0],
+                );
+                ui.selectable_value(
+                    &mut selected_wlan_interface,
+                    wlan_interfaces[1],
+                    wlan_interfaces[1],
+                );
+                ui.selectable_value(
+                    &mut selected_wlan_interface,
+                    wlan_interfaces[2],
+                    wlan_interfaces[2],
+                );
+            });
         });
     })
 }
